01e7f216b2e29437ee39e995c9a9f3ae46ed82dd
bazoon <vith@Mac-mini-VITALIY.local>
2013-09-17 12:55:34 +0600

    Добавлены свои исключения для обработки загрузки фото на конкурс

---
+++ lib/exceptions/exceptions.rb
@@ -0,0 +1,39 @@
+module Exceptions
+
+
+  class EmptyNomination < Exception
+
+    def initialize(msg = I18n.t(:nomination_empty))
+      super(msg)
+    end
+
+  end
+
+  class NoPhotoAttached < Exception
+    def initialize(msg = I18n.t(:photo_ids_empty))
+      super(msg)
+     end
+  end
+
+  class ClosedCompetition < Exception
+     def initialize(msg = I18n.t(:competition_too_late))
+      super(msg)
+     end
+  end
+
+  class DuplicatePhoto < Exception
+
+    def initialize(msg = I18n.t(:comp_photo_already_posted))
+      super(msg)
+    end
+
+  end
+
+  class MaxNominationCapacity < Exception
+    def initialize(msg = I18n.t(:max_nomination_capacity_exceeded))
+      super(msg)
+    end
+  end
+
+
+end
