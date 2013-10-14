fea0930091516ab1315e2d32b09459c46887265b
bazoon <vith@Mac-mini-VITALIY.local>
2013-10-14 16:59:42 +0600

    Fixed ip

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "admin@146.185.131.154"
+  config.mailer_sender = "admin@37.139.14.201"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
