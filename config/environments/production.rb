8c73c8c32e61b78cbbacf0d30dedbc286db3404b
bazoon <vith@Mac-mini-VITALIY.local>
2013-11-15 10:24:12 +0600

    Config.assets.css_compressor = :sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Photohome::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
