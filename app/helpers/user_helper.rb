e0efdc4fb27ecf4a8aa737f6544d0d3416b20430
bazoon <vith@Mac-mini-VITALIY.local>
2013-11-27 11:35:01 +0600

    Change badge color in letters

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,12 +2,12 @@ module UserHelper
   
 
   def incoming_mail_icon
-    
+  
   
 
     count = Letter.joins(:letter_users).where("seen = false and letter_users.user_id = ?",[current_user.id]).count
     if count > 0
-      content_tag(:span,count,class: "badge pull-right")
+      content_tag(:span,count,class: "pull-right label label-warning",style: "margin-left: 15px")
     else 
       ""
     end
