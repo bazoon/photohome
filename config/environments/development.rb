59d647eb416e2b9e256a5c5bcd5a79b3f385ee30
bazoon <vith@Mac-mini-VITALIY.local>
2013-11-27 09:47:58 +0600

    Пользователь видит стутус отправленных сообщений

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,7 @@ Photohome::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  config.time_zone = "Ekaterinburg"
 end
 
 
