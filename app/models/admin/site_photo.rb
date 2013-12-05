3f86928ef75105e58f3957cef95734e8d2157048
bazoon <vith@Mac-mini-VITALIY.local>
2013-12-05 16:49:34 +0600

    Site_photo bug with user == nil removed

--- app/models/admin/site_photo.rb
+++ app/models/admin/site_photo.rb
@@ -2,6 +2,7 @@ class Admin::SitePhoto < ActiveRecord::Base
  belongs_to :album, class_name: "Admin::Album"
  belongs_to :photo
 
+
  mount_uploader :image, AdminSitePhotoUploader
 
  alias :old_image_url :image_url
@@ -13,8 +14,8 @@ def image_url(size)
   photo_id.nil? ?  old_image_url(size) : photo.image_url(size)  
 end
 
-def author
-  photo_id.nil? ? "" : Photo.find(photo_id).user.name
+def user
+  photo_id.nil? ? "" : Photo.find(photo_id).user 
 end
 
 end
