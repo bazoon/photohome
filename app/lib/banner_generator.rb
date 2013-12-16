5218e5349d691350be445b2be012104952049912
bazoon <vith@Mac-mini-VITALIY.local>
2013-12-16 15:45:51 +0600

    Banners bug trying...

--- app/lib/banner_generator.rb
+++ app/lib/banner_generator.rb
@@ -4,13 +4,13 @@ class BannerGenerator
 
   def top
     banner = Admin::BannerPlacement.select_for_top
-    banner && banner.image_url
+    banner
   end
 
   def bottom
 
     banner = Admin::BannerPlacement.select_for_bottom
-    banner && banner.image_url
+    banner
 
   end
 
