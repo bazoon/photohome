d59ba1f7d3220049d2eb968f19b8c7f4df986715
bazoon <vith@Mac-mini-VITALIY.local>
2013-12-23 15:02:12 +0600

    Move secrets to env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAICEPCAQUMSUP6Z2A',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESSS"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET"],                        # required
     :region                 => 'eu-west-1'                  # optional, defaults to 'us-east-1'
    
   }
@@ -10,3 +10,4 @@ CarrierWave.configure do |config|
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
+
