7738dd547c52ecf21796b07932f44b8e1cdd21c1
bazoon <vith@Mac-mini-VITALIY.local>
2013-12-26 10:55:27 +0600

    Removed application.yml from git control

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,21 @@
 CarrierWave.configure do |config|
-    
+
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
-  else  
+  else
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV["AWS_ACCESSS"],                        # required
       :aws_secret_access_key  => ENV["AWS_SECRET"],                        # required
       :region                 => 'eu-west-1'                  # optional, defaults to 'us-east-1'
-     
+
     }
+    config.asset_host = "http://dxmbwukubv76f.cloudfront.net"
     config.fog_directory  = 'photohome'                     # required
-    config.fog_public     = false                                   # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-  end  
+    config.fog_public     = true                                   # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000','Expires' => 1.week.from_now.httpdate}  # optional, defaults to {}
+  end
 
 end
 
