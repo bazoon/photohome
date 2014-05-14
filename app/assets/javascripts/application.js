96360fe831053bb3280a38d82d4d951cda05ebba
vith <vitaliynesterenko@Mac-mini-VITALIY-2.local>
2014-05-14 15:13:42 +0600

    Layout tweaks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -82,51 +82,7 @@ $('.fotorama').on(
   }
 );
 
- $(".jury_estimate").jRating({
-         // step:true,
-         length : 10, // nb of stars
-         phpPath: "/jury_rating",
-         nbRates: 10,
-         type: "big",
-         rateMax: 10,
-         canRateAgain: true,  
-         decimalLength: 2  ,
-         
-         // onSuccess : function(){
-         //   alert('Success : your rate has been saved :)');
-         // },
-         // onError : function(){
-         //   alert('Error !');
-         // }
-         
-
-
-       });
-
-
- $(".final_estimate").jRating({
-         // step:true,
-         length : 3, // nb of stars
-         phpPath: "/admin/final_rating",
-         nbRates: 3,
-         type: "big",
-         rateMax: 3,
-         canRateAgain: true,  
-         step: true
-         // decimalLength: 1,
-         
-         // onSuccess : function(){
-         //   alert('Success : your rate has been saved :)');
-         // },
-         // onError : function(){
-         //   alert('Error !');
-         // }
-         
-
-
-       });
- 
-     
+    
 
 
 });
--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,5 +1,7 @@
 class AuthorController < ApplicationController
 
+	skip_before_filter :authenticate_user!
+
   def index
   	@users = User.all
   end
