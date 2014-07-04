489665e6ac2c15a39d4e8ded7800e70e05032d5b
bazoon <vith77@gmail.com>
2014-07-04 15:25:59 +0400

    Update devise.rb new email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "root@localhost"
+  config.mailer_sender = "activation@domfoto72.ru"
   config.secret_key = '217f686f52f8811ca760a03c19d74a5249ac547ce344c829f2b222b48dbf8147eda4df1432196d27c77da5b8030e9332229d962719b31c81d7857f146a9a87b8'
 
   # Configure the class responsible to send e-mails.
