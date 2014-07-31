7cc0b3aefbe64382a0356d5be5928b1ce2c09332
vith <vitaliynesterenko@Mac-mini-VITALIY-2.local>
2014-07-31 21:16:13 +0600

    Bug with migration

--- db/migrate/20140731142404_add_image_to_admin_setting.rb
+++ db/migrate/20140731142404_add_image_to_admin_setting.rb
@@ -1,6 +1,6 @@
 class AddImageToAdminSetting < ActiveRecord::Migration
   def change
-    add_column :admin_settings, :adult_image, :string
-    remove_column :admin_settings, :image
+    # add_column :admin_settings, :adult_image, :string
+    # remove_column :admin_settings, :image
   end
 end
