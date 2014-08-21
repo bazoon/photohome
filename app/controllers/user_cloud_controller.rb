3cbe9d13b40272093f05313532bff9e8e6278418
VITALIY NESTERENKO <vitaliynesterenko@MacBook-Pro-VITALIY.local>
2014-08-21 19:44:03 +0600

    Fixed tagging on user photos (включал в облако не свои фото)

--- app/controllers/user_cloud_controller.rb
+++ app/controllers/user_cloud_controller.rb
@@ -4,10 +4,11 @@ class UserCloudController < ApplicationController
 
    def cloud
     @user = User.friendly.find(params[:user_id])    
-    @tag_ids = ActsAsTaggableOn::Tagging.where("tagger_id=?",[@user.id]).map(&:tag_id)
-    @id_hash = @tag_ids.inject(Hash.new(0)) {|h,e| h[e] += 1; h}
-    @freqs = @id_hash.map {|e| [ ActsAsTaggableOn::Tag.find(e[0]).name , e[1] ] }
+    # @tag_ids = ActsAsTaggableOn::Tagging.where("tagger_id=?",[@user.id]).map(&:tag_id)
+    # @id_hash = @tag_ids.inject(Hash.new(0)) {|h,e| h[e] += 1; h}
+    # @freqs = @id_hash.map {|e| [ ActsAsTaggableOn::Tag.find(e[0]).name , e[1] ] }
 
+    @freqs = @user.photos.tag_counts_on(:themes).map {|t| [t.name,t.taggings_count]}
 
   end
 
