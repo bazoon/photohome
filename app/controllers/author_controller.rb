92e6bffa59eb9dada01ff974148442ef545c4e30
VITALIY NESTERENKO <vitaliynesterenko@MacBook-Pro-VITALIY.local>
2014-08-25 11:00:08 +0600

    Авторы упорядочены по алфавиту

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -3,7 +3,7 @@ class AuthorController < ApplicationController
 	skip_before_filter :authenticate_user!
 
   def index
-  	@users = User.where("photos_count > 0")
+  	@users = User.where("photos_count > 0").order(:last_name,:name)
   end
 
 
