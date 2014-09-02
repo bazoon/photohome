d87c4d13e92bbf7ef3433c9eae86e84a0787ed5c
VITALIY NESTERENKO <vitaliynesterenko@MacBook-Pro-VITALIY.local>
2014-09-02 09:15:32 +0600

    Починил link_to_function в choose_photo

--- app/models/competition_photo.rb
+++ app/models/competition_photo.rb
@@ -8,7 +8,8 @@ class CompetitionPhoto < ActiveRecord::Base
   attr_reader :photo_ids
 
   delegate :image_url,:title,to: :photo
-  delegate :image_url,:title,to: :photo
+  delegate :image_url_for_others,to: :photo
+
   has_many :jury_ratings
   has_many :likes
 
