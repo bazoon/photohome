81b281fc6886a784861e8f10376cc0d65310b43c
bazoon <vith77@gmail.com>
2014-11-14 11:46:12 +0500

    Sorting for comp forms

--- app/controllers/admin/request_forms_controller.rb
+++ app/controllers/admin/request_forms_controller.rb
@@ -3,6 +3,14 @@ class Admin::RequestFormsController < ApplicationController
   def show
     @competition = Competition.friendly.find(params[:competition_id])
     
+    @competition_requests = @competition.competition_requests.sort_by do |r|
+
+      [r.response_id, r.user.full_name]
+
+
+
+    end
+
   end
 
 end
