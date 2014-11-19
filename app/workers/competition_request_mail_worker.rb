1171172de6c73509ec444696a7e6a2dbcacc27a2
bazoon <vith77@gmail.com>
2014-11-19 21:50:35 +0500

    FIX4

--- app/workers/competition_request_mail_worker.rb
+++ app/workers/competition_request_mail_worker.rb
@@ -8,7 +8,7 @@ include Sidekiq::Worker
         competition_title = competition_request.competition.title
         decision = competition_request.decision
         answer = competition_request.answer
-        
+        puts competition_request.inspect
         UserMailer.message_email(competition_request.user, competition_title, decision, answer).deliver
     end
 
