87de33be852d71f8a2cf83d6a947091426c5ef52
bazoon <vith77@gmail.com>
2014-11-19 21:37:46 +0500

    FIX mail

--- app/lib/competition_request_notification.rb
+++ app/lib/competition_request_notification.rb
@@ -2,13 +2,14 @@ class CompetitionRequestNotification
 
   
   def self.send_mail(competition_request_id)
-    # CompetitionRequestMailWorker.perform_async(competition_request_id)
-    CompetitionRequestMailWorker.new.perform(competition_request_id)  
+    CompetitionRequestMailWorker.perform_async(competition_request_id)
+    # CompetitionRequestMailWorker.new.perform(competition_request_id)  
   end
 
   def self.send_letter(competition_request_id, user_id)
 
-    CompetitionRequestLetterWorker.perform_async(competition_request_id, user_id)
+    # CompetitionRequestLetterWorker.perform_async(competition_request_id, user_id)
+    CompetitionRequestLetterWorker.new.perform(competition_request_id, user_id)
 
 
   end
