e8aadb86a9139407bcebf246f0055c39bd7d20b0
bazoon <vith77@gmail.com>
2014-11-29 08:57:37 +0500

    Fix BUG with nomination_count!

--- photohome_start.sh
+++ photohome_start.sh
@@ -6,8 +6,8 @@
 #sidekiq
 cd /var/www/photohome
 /home/deployer/.rbenv/shims/bundle install
-/home/deployer/.rbenv/shims/bundle exec sidekiq -c 3 -d -L /var/www/photohome/log/sidekiq.log -e production
-
+#/home/deployer/.rbenv/shims/bundle exec sidekiq -c 3 -d -L /var/www/photohome/log/sidekiq.log -e production
+/home/deployer/.rbenv/shims/bundle exec sidekiq -c 3 -d -L /var/www/domfoto72/data/www/domfoto72.ru/log/sidekiq.log -C /var/www/domfoto72/data/www/domfoto72.ru/config/sidekiq.yml -e production
 
 #main
 /home/deployer/.rbenv/shims/puma -d -e production
