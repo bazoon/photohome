dd2fc5d64e50cbd014f636c525f0c60a488ba292
bazoon <vith77@gmail.com>
2014-12-05 19:11:20 +0500

    Coloring for recipients

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,73 +13,73 @@ class HomeController < ApplicationController
   end
 
 
-  def test2
+  # def test2
     
-    Mailboxer::Conversation.destroy_all
+  #   Mailboxer::Conversation.destroy_all
     
 
-    Letter.all.each do |letter|
+  #   Letter.all.each do |letter|
 
       
 
-      conversation = Mailboxer::Conversation.create!(subject: letter.title)
+  #     conversation = Mailboxer::Conversation.create!(subject: letter.title)
 
-      recipient_ids = [letter.user_id]
-      recipient_ids += letter.people.map(&:user_id)
+  #     recipient_ids = [letter.user_id]
+  #     recipient_ids += letter.people.map(&:user_id)
 
-      body = letter.content.empty? ? "..." : letter.content
-      subject = letter.title.empty? ? "..." : letter.title
+  #     body = letter.content.empty? ? "..." : letter.content
+  #     subject = letter.title.empty? ? "..." : letter.title
 
-      notification = Mailboxer::Notification.create!(type: 'Mailboxer::Message',
-                    body: body ,sender_id: letter.user_id, subject: subject,
-                    sender_type: 'User', conversation_id: conversation.id)
+  #     notification = Mailboxer::Notification.create!(type: 'Mailboxer::Message',
+  #                   body: body ,sender_id: letter.user_id, subject: subject,
+  #                   sender_type: 'User', conversation_id: conversation.id)
 
       
 
-      Mailboxer::Receipt.create!(receiver_id: letter.user_id,
-                  receiver_type: 'User', notification_id: notification.id,
-                  mailbox_type: 'sentbox')
+  #     Mailboxer::Receipt.create!(receiver_id: letter.user_id,
+  #                 receiver_type: 'User', notification_id: notification.id,
+  #                 mailbox_type: 'sentbox')
 
 
-      letter.people.all.each do |person|
+  #     letter.people.all.each do |person|
 
-        Mailboxer::Receipt.create!(receiver_id: person.user_id,
-                  receiver_type: 'User', notification_id: notification.id,
-                  mailbox_type: 'inbox')
-      end
+  #       Mailboxer::Receipt.create!(receiver_id: person.user_id,
+  #                 receiver_type: 'User', notification_id: notification.id,
+  #                 mailbox_type: 'inbox')
+  #     end
 
-      letter.comments.all.each do |comment|
+  #     letter.comments.all.each do |comment|
 
-        body = comment.comment.empty? ? "..." : comment.comment
-        subject = letter.title.empty? ? "..." : letter.title
+  #       body = comment.comment.empty? ? "..." : comment.comment
+  #       subject = letter.title.empty? ? "..." : letter.title
 
-        notification = Mailboxer::Notification.create!(type: 'Mailboxer::Message', 
-                    body: body, sender_id: comment.user_id, subject: subject,
-                    sender_type: 'User', conversation_id: conversation.id)
+  #       notification = Mailboxer::Notification.create!(type: 'Mailboxer::Message', 
+  #                   body: body, sender_id: comment.user_id, subject: subject,
+  #                   sender_type: 'User', conversation_id: conversation.id)
 
-        Mailboxer::Receipt.create!(receiver_id: comment.user.id,
-                    receiver_type: 'User', notification_id: notification.id,
-                    mailbox_type: 'sentbox')
+  #       Mailboxer::Receipt.create!(receiver_id: comment.user.id,
+  #                   receiver_type: 'User', notification_id: notification.id,
+  #                   mailbox_type: 'sentbox')
 
-        recipient_ids.each do |recipient_id|
+  #       recipient_ids.each do |recipient_id|
           
-            Mailboxer::Receipt.create!(receiver_id: recipient_id,
-                    receiver_type: 'User', notification_id: notification.id,
-                    mailbox_type: 'inbox') if recipient_id != comment.user_id
-            # binding.pry
+  #           Mailboxer::Receipt.create!(receiver_id: recipient_id,
+  #                   receiver_type: 'User', notification_id: notification.id,
+  #                   mailbox_type: 'inbox') if recipient_id != comment.user_id
+  #           # binding.pry
           
-        end
+  #       end
 
 
 
-      end
+  #     end
 
 
 
-    end
+  #   end
 
 
-  end
+  # end
 
 
 end
