7d5779dcbcd7e069409de280c883dcb0e43dddfa
bazoon <vith77@gmail.com>
2015-01-29 14:05:00 +0500

    Fix

--- app/controllers/competitions/best_controller.rb
+++ app/controllers/competitions/best_controller.rb
@@ -6,7 +6,7 @@ class Competitions::BestController < ApplicationController
                                       .not_banned
                                       .to_a.sort_by { |cp| [-cp.average_rating, cp.place == 0 ? 1000: cp.place] }
                                       .paginate(page: params[:page], per_page: 8)
-    @start_num = params[:page].to_i*8 + 1
+    @start_num = (params[:page].to_i-1)*8 + 1
   end
 
 
