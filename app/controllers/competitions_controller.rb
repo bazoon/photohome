e2586f1cfeaaf5790ba725aba6b0f4194d6af210
bazoon <vith77@gmail.com>
2015-03-18 17:10:07 +0500

    Fix skip filter

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,5 @@
 class CompetitionsController < ApplicationController
-  # skip_before_filter :authenticate_user!, only: [:index, :view_photos, :show, :view_nominations, :results_nominations, :results]
+  skip_before_filter :authenticate_user!
   before_action :set_competition, except: [:index]
 
   # GET /competitions
