ee753a90673e2df6c86e53cc757786fa4a6713cd
bazoon <vith77@gmail.com>
2018-02-13 21:59:22 +0500

    Починка правки профиля

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,15 +6,29 @@ class RegistrationsController < Devise::RegistrationsController
   # layout "user_profile_layout", except: [:new]
 
   def update_sanitized_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :login, :about, :email, :country, :password, :password_confirmation,:avatar,:terms_of_service, :birth_date])
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:name, :last_name, :login, :about, :adress, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :last_name, :login, :about, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :last_name, :login, :about, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation)
+    end
+    
   end
+
+
+  
 
 
  protected
 
-    def after_update_path_for(resource)
-      user_profile_path(resource)
-    end
+    # def after_update_path_for(resource)
+    #   user_profile_path(resource)
+    # end
 private
   def check_captcha
     if verify_recaptcha
