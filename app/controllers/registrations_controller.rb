7215a1647054dd5edc7e5f86f92b520b60e68d02
bazoon <vith77@gmail.com>
2018-02-16 12:07:29 +0500

    Поля

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,15 +7,15 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update_sanitized_params
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:name, :last_name, :login, :about, :adress, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation)
+      user_params.permit(:name, :last_name, :login, :about, :adress, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation, :zip_code, :city, :phone)
     end
 
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:name, :last_name, :login, :about, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation)
+      user_params.permit(:name, :last_name, :login, :about, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation, :zip_code, :city, :phone)
     end
 
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:name, :last_name, :login, :about, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation)
+      user_params.permit(:name, :last_name, :login, :about, :email, :country,:avatar,:terms_of_service, :birth_date,:current_password, :password, :password_confirmation, :zip_code, :city, :phone)
     end
     
   end
